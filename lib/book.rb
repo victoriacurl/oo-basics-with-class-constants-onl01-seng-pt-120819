a90a2a7d51e17ad194ac1187775e02c851aebe13
Victoria Curl <victoriacurl9@gmail.com>
2020-01-17 18:39:11 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,8 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count 
+  attr_reader :title, :genre
+
+  GENRES = []
 
   def initialize(title)
     @title = title
